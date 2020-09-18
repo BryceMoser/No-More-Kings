d18d6112032264304cb9c254989af3fca1e09a76
BryceMoser <brycemoser@yahoo.com>
2020-09-17 23:38:32 -0700

    Removed assets due to error in scripting

---
+++ No More Kings!/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float forwardInput;
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] float turnSpeed = 25.0f;
+    [SerializeField] int bagCapacity = 10;
+    public int currentBagSize;
+
+    private void Start()
+    {
+        currentBagSize = 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Move the vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates the vehicle based on horizontal input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
