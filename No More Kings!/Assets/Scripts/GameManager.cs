ef67c34f3a82756ce292417c915a5da4c726b653
BryceMoser <brycemoser@yahoo.com>
2020-09-21 13:14:10 -0700

    Housing and Spawn Positions
    Housing is set up, as well as locations for mail to spawn in front of houses. Player is able to move through city, while colliding with houses and successfully triggering mail item.

--- No More Kings!/Assets/Scripts/GameManager.cs
+++ No More Kings!/Assets/Scripts/GameManager.cs
@@ -4,9 +4,36 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject mail;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Entering specific transform locations for all spawn locations
+        Vector3[] positions = new Vector3[]
+        {
+            new Vector3(-173f, 0f, -222f),
+            new Vector3(-173f, 0f, -323f),
+            new Vector3(-173f, 0f, -421.5f),
+            new Vector3(123f, 0f, -222f),
+            new Vector3(123f, 0f, -323f),
+            new Vector3(123f, 0f, -421.5f),
+            new Vector3(-131f, 0f, -280f),
+            new Vector3(-75f, 0f, -280f),
+            new Vector3(19f, 0f, -280f),
+            new Vector3(75f, 0f, -280f),
+            new Vector3(-131f, 0f, -385f),
+            new Vector3(-75f, 0f, -385f),
+            new Vector3(19f, 0f, -385f),
+            new Vector3(75f, 0f, -385f),
+            new Vector3(-131f, 0f, -480f),
+            new Vector3(-75f, 0f, -480f),
+            new Vector3(19f, 0f, -480f),
+            new Vector3(75f, 0f, -480f)
+
+        };
+
+        Instantiate(mail, positions[0], mail.transform.rotation);
         
     }
 
