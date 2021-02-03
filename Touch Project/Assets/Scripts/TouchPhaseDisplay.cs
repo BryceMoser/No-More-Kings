040d92b3a72e399507feea7acb17b208b613dc46
BryceMoser <brycemoser@yahoo.com>
2021-02-03 14:31:52 -0800

    Updates

---
+++ Touch Project/Assets/Scripts/TouchPhaseDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchPhaseDisplay : MonoBehaviour
+{
+    public Text phaseDisplayText;
+    private Touch theTouch;
+    private float timeTouchEnded;
+    private float displayTime = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If there is at least one finger touching
+        if(Input.touchCount > 0)
+        {
+            //Set the first touch to theTouch
+            theTouch = Input.GetTouch(0);
+            if(theTouch.phase == TouchPhase.Ended)
+            {
+                phaseDisplayText.text = theTouch.phase.ToString();
+                timeTouchEnded = Time.time;
+            }
+
+            else if (Time.time - timeTouchEnded > displayTime)
+            {
+                phaseDisplayText.text = theTouch.phase.ToString();
+                timeTouchEnded = Time.time;
+            }
+        }
+        else if (Time.time - timeTouchEnded > displayTime)
+        {
+            phaseDisplayText.text = "";
+        }
+    }
+}
---
+++ Touch Project/Assets/Scripts/VirtualDPad.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VirtualDPad : MonoBehaviour
+{
+    public Text directionText;
+    private Touch theTouch;
+    private Vector2 touchStartPosition, touchEndPositiion;
+    private string direction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            theTouch = Input.GetTouch(0);
+
+            //If the touch has just started, make the current touch the starting position
+            if(theTouch.phase == TouchPhase.Began)
+            {
+                touchStartPosition = theTouch.position;
+            }
+
+            //If touch is either moving or ending, calculate with direction touch moved
+            else if(theTouch.phase == TouchPhase.Moved || theTouch.phase == TouchPhase.Ended)
+            {
+                touchEndPositiion = theTouch.position;
+
+                float x = touchEndPositiion.x - touchStartPosition.x;
+                float y = touchEndPositiion.y - touchStartPosition.y;
+
+                //If there is no difference, the player only tapped the screen
+                if(Mathf.Abs(x) == 0 && Mathf.Abs(y) == 0)
+                {
+                    direction = "Tapped";
+                }
+
+                else if(Mathf.Abs(x) > Mathf.Abs(y))
+                {
+                    direction = x > 0 ? "Right" : "Left";
+                }
+                else
+                {
+                    direction = y > 0 ? "Up" : "Down";
+                }
+            }
+        }
+        directionText.text = direction;
+    }
+}
